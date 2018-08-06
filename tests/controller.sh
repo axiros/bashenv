342b3f02c52b39469d77be42459229f87ab55952
GK <GK@axiros.com>
2018-08-06 18:23:14 +0200

    paths

--- tests/controller.sh
+++ tests/controller.sh
@@ -6,28 +6,45 @@ here="$(unset CDPATH && cd "$(dirname "$BASH_SOURCE")" && echo $PWD)"
 cd "$here"
 cd ..
 
-test_conda_prefix="`pwd`/xc2"
+P1="/tmp/xc"
+P2="/tmp/xc2"
 create="misc/create_bashenv_kickstart/create"
 
+syspath="$PATH"
+
 remove_existing () {
-    rm -rf "$test_conda_prefix" || true
+    rm -rf "$1" || true
+}
+
+verify () {
+    export PATH="$1/bin:$syspath"
+    which python | grep "$1" || exit 1
+    which hg          | grep "$1" || exit 1
+    which git         | grep "$1" || exit 1
+    which constructor | grep "$1" || exit 1
+    test -e "$1/.git" || exit 1
 }
 
 test_create_scratch () {
-    $create -p "$test_conda_prefix" go
-    export PATH="$test_conda_prefix/bin:$PATH"
-    which python | grep "$test_conda_prefix" || exit 1
-    which hg          | grep "$test_conda_prefix" || exit 1
-    which git         | grep "$test_conda_prefix" || exit 1
-    which constructor | grep "$test_conda_prefix" || exit 1
-    test -e "$test_conda_prefix/.git" || exit 1
+    remove_existing "$P1"
+    $create -p "$P1" go
+    verify "$P1"
 
 }
 
 
+test_create_from_existing () {
+    remove_existing "$P2"
+    p="$PATH"
+    source "$P1/bin/app/environ/bash/be_active"
+    nfo "have it"
+    "$P1/bin/app/environ/bash/$create" -bp "$P2" go
+    verify "$P2"
+}
+
 main () {
-    remove_existing
-    test_create_scratch
+    ( test_create_scratch; )
+    ( test_create_from_existing; )
 }
 
 main $*
