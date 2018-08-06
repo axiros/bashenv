7f45189151ef386b2800bc125fc74fedf93bbc5e
GK <GK@axiros.com>
2018-08-06 16:40:32 +0200

    create

--- tests/controller.sh
+++ tests/controller.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-set -xeu
+set -eu
+
+here="$(unset CDPATH && cd "$(dirname "$BASH_SOURCE")" && echo $PWD)"
+cd "$here"
+cd ..
 
 test_conda_prefix="`pwd`/xc2"
 create="misc/create_bashenv_kickstart/create"
@@ -11,7 +15,13 @@ remove_existing () {
 
 test_create_scratch () {
     $create -p "$test_conda_prefix" go
-    which python | grep "$test_conda_prefix"
+    export PATH="$test_conda_prefix/bin:$PATH"
+    which python | grep "$test_conda_prefix" || exit 1
+    which hg          | grep "$test_conda_prefix" || exit 1
+    which git         | grep "$test_conda_prefix" || exit 1
+    which constructor | grep "$test_conda_prefix" || exit 1
+    test -e "$test_conda_prefix/.git" || exit 1
+
 }
 
 
