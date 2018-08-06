29b2338e3ef51dc6d2bd014c9d0c29a483217a69
GK <GK@axiros.com>
2018-08-06 15:51:33 +0200

    prefix

---
+++ tests/controller.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -xeu
+
+test_conda_prefix="/xc2"
+create="misc/create_bashenv_kickstart/create"
+
+remove_existing () {
+    rm -rf "$test_conda_prefix" || true
+}
+
+test_create_scratch () {
+    $create -xAa -p "$test_conda_prefix" go
+    which python | grep "$test_conda_prefix"
+}
+
+
+main () {
+    remove_existing
+    test_create_scratch
+}
+
+main $*
